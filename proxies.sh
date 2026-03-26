ababf18e43b87f756178923aab6c172dd6bcea90
Mikaël Popowicz <mikael.popowicz@gmail.com>
2026-03-26 13:06:16 +0100

    Added valet proxies and updated the installation script with additional dependencies and tools.

---
+++ proxies.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+valet proxy ace-contracts http://localhost:8390
+valet proxy afr http://localhost:8890
+valet proxy api.b2b http://localhost:8192
+valet proxy api.balenciaga http://localhost:7872
+valet proxy api.fam http://localhost:8581
+valet proxy api.qvdf http://localhost:8041
+valet proxy api.tada http://localhost:8480
+valet proxy b2b http://localhost:3081
+valet proxy babcock http://localhost:8580
+valet proxy backend.b2b http://localhost:8191
+valet proxy backend.balenciaga http://localhost:7871
+valet proxy backend.edrh http://localhost:8091
+valet proxy backend.fam http://localhost:8580
+valet proxy backend.lb http://localhost:8880
+valet proxy backend.qvdf http://localhost:8040
+valet proxy backend.vp http://localhost:7180
+valet proxy balenciaga-public-expo-pdf-app http://localhost:8118
+valet proxy keycloak.babcock http://localhost:8589
+valet proxy qvdf http://localhost:3080
+valet proxy tada http://localhost:8480
